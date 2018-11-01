c896dcd31d71e5c2e548aeef9252dd2ee5f82875
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-11-01 16:26:56 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env/python 
+
+from setuptools import setup 
+
+setup(name='metabolishmm',
+    version='0.1',
+    description='Tool for summarizing metabolism of genomes',
+    url='https://github.com/elizabethmcd/MetabolisHMM',
+    author='Elizabeth McDaniel',
+    author_email='emcdaniel@wisc.edu',
+    packages=['metabolishmm'],
+    zip_safe=False)
