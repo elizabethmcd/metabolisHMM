d1957ed304addcc20329260c03b8a8401a7a1a04
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-11-20 13:27:15 -0600

    reorganizing how pipeline will run, also added ribosomal markers for making genome phylogenies

--- metabolishmm/_init_.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env/ python 
-
-import os 
-
-def version():
-    """Read program version from file."""
-    version_file = open(os.path.join_path_[0], "VERSION")
-    return version_file.readline.strip()
