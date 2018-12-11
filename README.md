f3b536d856eddf3fa9ff5ab4e4dd8d4dd3c4cef1
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-12-11 15:40:26 -0600

    have concatenated fastas for each ribo marker hit

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ A tool for summarizing the metabolic capabilities and creating phylogenies of a 
 - HMMer
 - Muscle
 
-To run the pipeline, clone the repository with `git clone https://github.com/elizabethmcd/metabolisHMM`. This will provide you with the necessary scripts and HMM markers to run the analysis on a given set of genomes. Place the protein files ending in the extension `.faa` in a folder named `genomes`. 
+To run the pipeline, clone the repository with `git clone https://github.com/elizabethmcd/metabolisHMM`. This will provide you with the necessary scripts and HMM markers to run the analysis on a given set of genomes. Place the protein files ending in the extension `.faa` in a folder named `genomes`. If your filenames have dashes `-` in them, rename them for example with underscores instead, with `rename 's/-/_/g' *`.  
 
 To get metabolic summaries of your genomes, run `python run-metabolic-markers.py`. This will create a summary table of each metabolic marker and how many hits were found above the given threshold in all of your genomes. To create a genome phylogeny on the given genomes based on ribosomal protein markers, run `python run-ribosomal-markers.py`. Metabolic characterization summaries are provided in the `results` folder, and columns are ordered by functions *which will be added soon*. 
 
@@ -24,8 +24,12 @@ This tool is still under active development, and may contain bugs here and there
 
 Some of the markers were made publicly available from Karthik Anantharaman's study on [Thousands of microbial genomes shed light on interconnected biochemical processes in an aquifer system](https://www.nature.com/articles/ncomms13219) found [here](https://github.com/kanantharaman/metabolic-hmms). Certainly using HMM profiles to characterize metabolic capabilities predates this publication, but if you wanted you could cite this paper as a proof of practice. 
 
-1. Thousands of microbial genomes shed light on interconnected biogeochemical processes in an aquifer system. K. Anantharaman et al. _Nature Communications_, 2016. 
+1. Thousands of microbial genomes shed light on interconnected biogeochemical processes in an aquifer system. K. Anantharaman et al. _Nature Communications_. 2016. 
 
 Additionally if you add a new marker to the database, and your analysis heavily focusses on that marker (e.g. nitrogen cycling), you may want to cite some reference about that marker protein. 
 
-The code and HMM profiles are free to use for any purposes. Feel free to submit an issue with any questions or something I glaringly did wrong! 
+The ribosomal protein markers were also used for the expanded view of the tree of life from Hug et al.:
+
+2. A new view of the tree of life. L. Hug et al. _Nature Microbiology_. 2016.
+
+The code and released HMM profiles are free to use for any purposes. Feel free to submit an issue with any questions or something I glaringly did wrong! 
--- run-ribosomal-markers.py
+++ run-ribosomal-markers.py
@@ -5,59 +5,48 @@ import subprocess
 import os, sys
 import pandas as pd 
 from Bio import SeqIO, SearchIO
-from Bio.Nexus import Nexus
 
 # Setup
-genomes = glob.glob("genomes/*.faa")
-ribos = glob.glob("ribosomal_markers/*.hmm")
-os.mkdir("out")
-os.mkdir("results")
-FNULL = open(os.devnull, 'w')
+# genomes = glob.glob("genomes/*.faa")
+# ribos = glob.glob("ribosomal_markers/*.hmm")
+# os.mkdir("out")
+# os.mkdir("results")
+# FNULL = open(os.devnull, 'w')
 
-# setup hmmsearch run
-for genome in genomes:
-    name=os.path.basename(genome).replace(".faa", "").strip().splitlines()[0]
-    dir=name
-    os.mkdir("out/"+dir)
-    for ribo in ribos: 
-        prot=os.path.basename(ribo).replace(".hmm", "").replace("_bact", "").strip().splitlines()[0]
-        outname= "out/"+dir+"/"+name + "-" + prot + ".out"
-        cmd = ["hmmsearch", "--tblout="+outname, ribo, genome]
-        subprocess.call(cmd, stdout=FNULL)
-        print("Running HMMsearch on " + name + " and " + prot + " marker")
+# # setup hmmsearch run
+# for genome in genomes:
+#     name=os.path.basename(genome).replace(".faa", "").strip().splitlines()[0]
+#     dir=name
+#     os.mkdir("out/"+dir)
+#     for ribo in ribos: 
+#         prot=os.path.basename(ribo).replace(".hmm", "").replace("_bact", "").strip().splitlines()[0]
+#         outname= "out/"+dir+"/"+name + "-" + prot + ".out"
+#         cmd = ["hmmsearch", "--tblout="+outname, ribo, genome]
+#         subprocess.call(cmd, stdout=FNULL)
+#         print("Running HMMsearch on " + name + " and " + prot + " marker")
 
-# parsing the hmm output file and get into results file
-
+# Parse HMM outputs
+prot_list=['rpL14', 'rpL15', 'rpL16', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6', 'rpS10', 'rpS17', 'rpS19', 'rpS3', 'rpS8', 'rpL14', 'rpL15', 'rpL16', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6', 'rpS10', 'rpS17', 'rpS19', 'rpS3', 'rpS8']
 result_dirs = os.walk("out/")
-output = ("results/all-results-loci.txt")
-with open(output, "a") as f:
-    for path, dirs, files in result_dirs:
+for prot in prot_list:
+    for path, dirs, files in result_dirs: 
         for file in files:
-            genome = file.split("-")[0]
-            prot = file.replace(".out", "").split("-")[1]
-            result = "out/"+genome+"/"+file
-            with open(result, "rU") as input:
-                for qresult in SearchIO.parse(input, "hmmer3-tab"):
-                    hits = qresult.hits
-                    num_hits = len(hits)
-                    if num_hits > 0:
-                        for i in range(0, 1): # takes only the first, best hit
-                            hit_id = hits[i].id
-                            f.write("genomes/"+genome+".faa" + "\t" + prot + "\t" + hit_id + "\n")
+            genome=file.split("-")[0]
+            marker=file.replace(".out", "").split("-")[1]
+            result="out/"+genome+"/"+file
+            output="results/"+marker+".faa"
+            genome_file = "genomes/"+genome+".faa"
+            with open(output, "a") as outf:
+                with open(genome_file, "r") as input_fasta:
+                    with open(result, "rU") as input:
+                        for qresult in SearchIO.parse(input, "hmmer3-tab"):
+                            hits=qresult.hits
+                            num_hits=len(hits)
+                            if num_hits >0:
+                                for i in range(0, 1):
+                                    hit_id=hits[i].id
+                            for record in SeqIO.parse(input_fasta, "fasta"):
+                                if record.id in hit_id:
+                                    outf.write(">"+genome+"_"+hit_id+"\n"+str(record.seq)+"\n")
 
-# Parse locus tags from results file, get sequences 
-loci_list = pd.read_table("results/all-results-loci.txt", sep="\t", names=['genome', 'marker', 'locus'])
-loci=loci_list['locus']
-names=set(loci_list['genome'])
-markers=loci_list['marker']
-for marker in markers:
-    output = "results/"+marker+".faa"
-    with open(output, "a") as outfile:
-        for genome in genomes:
-            for record in SeqIO.parse(genome, "fasta"):
-                for locus in loci:
-                    if record.id in locus:
-                        outfile.write(">"+name+"_"+record.id+"\n")
-            
-
-# Make alignment file
+# Make alignments for each marker
