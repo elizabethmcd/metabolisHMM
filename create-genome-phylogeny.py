4205010769122229a5139d6c2a4bbc203a2d1ac8
Elizabeth McDaniel <emcdaniel@wisc.edu>
2019-09-10 21:37:36 -0500

    major updates to genome phylogeny script: have own concatenation step, fixed output directories, cleaned up lists

--- create-genome-phylogeny.py
+++ create-genome-phylogeny.py
@@ -1,76 +1,85 @@
 #! /usr/bin/env python 
 
-import glob
-import argparse
-import subprocess 
-import os, sys
-import pandas as pd 
-from Bio import SeqIO, SearchIO
+import glob, argparse, subprocess, os, sys, tempfile
+from Bio import BiopythonExperimentalWarning
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', BiopythonExperimentalWarning)
+    from Bio import SearchIO, SeqIO, AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.Seq import UnknownSeq, Seq
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
 
 # Arguments
-
-parser = argparse.ArgumentParser(description = "Create full archaeal/bacterial genome phylogenies based off specific ribosomal protein markers")
-parser.add_argument('--genome_dir', metavar='GENOMEDIR', help='Directory where genomes to be screened are held')
-parser.add_argument('--domain', metavar='DOMAIN', help='archaea, bacteria (choose one, need to be assessed separately)')
-parser.add_argument('--phylogeny', metavar='PHY', help="fastree or raxml, choose one")
-parser.add_argument("--threads",metavar='THREADS',help="number of threads for tree making")
+parser = argparse.ArgumentParser(description = "Create ribosomal phylogenies using specific ribosomal markers for archaea and/or bacteria")
+parser._action_groups.pop()
+required = parser.add_argument_group("required arguments")
+optional = parser.add_argument_group("optional arguments")
+required.add_argument('--input', metavar='INPUT', help='Directory where genomes to be screened are held')
+required.add_argument('--output', metavar='OUTPUT', help='Directory to store results and intermediate files')
+required.add_argument('--domain', metavar='DOMAIN', help='archaea, bacteria, all')
+required.add_argument('--phylogeny', metavar='PHY', help='fastree, raxml')
+optional.add_argument('--threads',metavar='THREADS',help='Optional: number of threads for calculating a tree using RAxML. This is not taken into account using Fastree')
 
 args = parser.parse_args()
-GENOMEDIR = args.genome_dir
-GENOMEFILES = GENOMEDIR + "/*.faa"
+GENOMEDIR = args.input
+GENOMEFILES = args.input + "/*.faa"
 DOMAIN = args.domain
 PHYTOOL = args.phylogeny
 THREADS = args.threads
+OUTPUT = args.output
+out_intm = OUTPUT + "/out"
+out_results = OUTPUT + "/results"
 
 # Setup
 genomes = glob.glob(GENOMEFILES)
-os.mkdir("out")
-os.mkdir("results")
+os.makedirs(out_intm)
+os.makedirs(out_results)
 FNULL = open(os.devnull, 'w')
 
+# different ribosomal markers for archaea/bacteria/all
 bacteria_list = ['rpL14','rpL15','rpL16','rpL18','rpL22','rpL24','rpL2','rpL3','rpL4','rpL5','rpL6','rpS10','rpS17','rpS19','rpS3','rpS8']
 archaea_list = ['rpL14','rpL15','rpL18','rpL22','rpL24','rpL2','rpL3','rpL4','rpL5','rpL6','rpS17','rpS19','rpS3','rpS8']
+all_list = ['rpL14','rpL15','rpL18','rpL22','rpL24','rpL2', 'rpL3','rpL4','rpL5','rpL6','rpS17','rpS19','rpS3','rpS8']
 
-# setup hmmsearch run for archaea or bacteria
-if DOMAIN == 'archaea':
-    for genome in genomes:
-        name=os.path.basename(genome).replace(".faa", "").strip().splitlines()[0]
-        dir=name
-        os.mkdir("out/"+dir)
-        for prot in archaea_list:
-            marker ="ribosomal_markers/"+prot+"_bact.hmm"
-            outname= "out/"+dir+"/"+name + "-" + prot + ".out"
-            cmd = ["hmmsearch", "--tblout="+outname, marker, genome]
-            subprocess.call(cmd, stdout=FNULL)
-            print("Running HMMsearch on " + name + " and " + prot + " marker")
+if DOMAIN == 'archaea':    
+    prot_list=archaea_list
 elif DOMAIN == 'bacteria':
-    for genome in genomes:
-        name=os.path.basename(genome).replace(".faa", "").strip().splitlines()[0]
-        dir=name
-        os.mkdir("out/"+dir)
-        for prot in bacteria_list:
-            marker="ribosomal_markers/"+prot+"_bact.hmm"
-            outname= "out/"+dir+"/"+name + "-" + prot + ".out"
-            cmd = ["hmmsearch", "--tblout="+outname, marker, genome]
-            subprocess.call(cmd, stdout=FNULL)
-            print("Running HMMsearch on " + name + " and " + prot + " marker")
-    
+    prot_list=bacteria_list
+elif DOMAIN == 'all': 
+    prot_list=all_list
+
+# setup hmmsearch run depending on HMM list
+print("Running ribosomal protein HMM searches...")
+for genome in genomes:
+    name=os.path.basename(genome).replace(".faa", "").strip().splitlines()[0]
+    dir=name
+    os.makedirs(OUTPUT+"/out/"+dir)
+    for prot in prot_list:
+        marker ="ribosomal_markers/"+prot+"_bact.hmm"
+        outname= OUTPUT + "/out/"+dir+"/"+name + "-" + prot + ".out"
+        cmd = ["hmmsearch", "--tblout="+outname, marker, genome]
+        subprocess.call(cmd, stdout=FNULL)
+
 # Parse HMM outputs
 print("Parsing results...")
 if DOMAIN == 'archaea':    
-    prot_list=['rpL14', 'rpL15', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6','rpS17', 'rpS19', 'rpS3', 'rpS8', 'rpL14', 'rpL15', 'rpL16', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6', 'rpS10', 'rpS17', 'rpS19', 'rpS3', 'rpS8']
-else:
-    prot_list=['rpL14', 'rpL15', 'rpL16', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6', 'rpS10', 'rpS17', 'rpS19', 'rpS3', 'rpS8', 'rpL14', 'rpL15', 'rpL16', 'rpL18', 'rpL2', 'rpL22', 'rpL24', 'rpL3', 'rpL4', 'rpL5', 'rpL6', 'rpS10', 'rpS17', 'rpS19', 'rpS3', 'rpS8']
-result_dirs = os.walk("out/")
+    prot_list=archaea_list
+elif DOMAIN == 'bacteria':
+    prot_list=bacteria_list
+elif DOMAIN == 'all': 
+    prot_list=all_list
+result_dirs = os.walk(OUTPUT +"/out/")
 for prot in prot_list:
     for path, dirs, files in result_dirs: 
         for file in files:
             genome=file.split("-")[0]
             marker=file.replace(".out", "").split("-")[1]
-            result="out/"+genome+"/"+file
-            output="results/"+marker+".faa"
+            result=OUTPUT + "/out/"+genome+"/"+file
+            outfasta=OUTPUT + "/results/"+marker+".faa"
             genome_file = GENOMEDIR+genome+".faa"
-            with open(output, "a") as outf:
+            with open(outfasta, "a") as outf:
                 with open(genome_file, "r") as input_fasta:
                     with open(result, "r") as input:
                         for qresult in SearchIO.parse(input, "hmmer3-tab"):
@@ -83,35 +92,55 @@ for prot in prot_list:
                                     if record.id in hit_id:
                                         outf.write(">"+genome+"\n"+str(record.seq)+"\n")
                        
-# Make alignment file
-print("Aligning concatenated hits...")
-fastas = glob.glob("results/*.faa")
+# Make alignment file for each marker
+print("Aligning ribosomal hits...")
+prots = OUTPUT + "/results/*.faa"
+fastas = glob.glob(prots)
 for fasta in fastas:
     outname = os.path.basename(fasta).replace(".faa", "").strip().splitlines()[0]
-    output= "results/"+outname+".aln"
+    output= OUTPUT + "/results/"+outname+".aln"
     mafft_cmd = "mafft --quiet "
     mafft_cmd += fasta+" > "+output
     os.system(mafft_cmd)
 
 # Concatenate alignments
 print("Concatenating alignments...")
-alignments="results/*.aln"
-outname="results/"+DOMAIN+"-ribo-concatenated-phylogeny.fasta"
-cat_cmd = "catfasta2phyml.pl -f --concatenate "
-cat_cmd += alignments+" > "+outname
-os.system(cat_cmd)
+# referred to the biopython cookbook for concatenating multiple sequence alignments 
+# adds questions marks for missing loci for a given genome
+prot_alignments = OUTPUT + "/results/*.aln"
+infiles = glob.glob(prot_alignments)
+concatout = OUTPUT + "/out/"+DOMAIN+"-concatenated-ribosomal-alignment.fasta"
+alignments = [AlignIO.read(open(f, "r"), "fasta") for f in infiles]
+all_labels = set(seq.id for aln in alignments for seq in aln)
+tmp = defaultdict(list)
+for aln in alignments:
+    length = aln.get_alignment_length()
+    these_labels = set(rec.id for rec in aln)
+    missing = all_labels - these_labels
+    for label in missing:
+        new_seq = UnknownSeq(length)
+        tmp[label].append(str(new_seq))
+    for rec in aln:
+        tmp[rec.id].append(str(rec.seq))
+msa = MultipleSeqAlignment(SeqRecord(Seq(''.join(v)), id=k)
+            for (k,v) in tmp.items())
+AlignIO.write(msa,concatout,"fasta")
+
+# get rid of "unknown description" descriptor in alignment file that biopython adds and I haven't figured out how to remove
+reformatout=OUTPUT + "/results/"+DOMAIN+"-concatenated-ribosomal-alignment-reformatted.fasta"
+filter_cmd = "sed 's/<unknown description>//g' "+concatout+" > "+reformatout
+os.system(filter_cmd)
 
 # Create tree
 if PHYTOOL == 'fastree':
     print("Calculating tree using FastTree...")
-    fileIn="results/"+DOMAIN+"-ribo-concatenated-phylogeny.fasta"
-    outname = "results/"+DOMAIN+"-fastTree-ribosomal-tree.tre"
-    fastCmd = "FastTree "+fileIn+" > "+outname
+    fileIn=OUTPUT + "/results/"+DOMAIN+"-concatenated-ribosomal-alignment-reformatted.fasta"
+    outname = OUTPUT + "/results/"+DOMAIN+"-fastTree-ribosomal-tree.tre"
+    fastCmd = "FastTree -quiet "+fileIn+" > "+outname
     os.system(fastCmd)
 elif PHYTOOL == "raxml":
     print("Calculating tree with RaxML... be patient...")
     outname= DOMAIN+"-raxml-ribo"
-    outDir = "results/"
-    fileIn="results/"+DOMAIN+"-ribo-concatenated-phylogeny.fasta"
+    fileIn=OUTPUT + "/results/"+DOMAIN+"concatenated-ribosomal-alignment-reformatted.fasta"
     raxCmd = "raxmlHPC-PTHREADS -f a -m PROTGAMMAAUTO -p 12345 -x 12345 -# 100 -s "+fileIn+" -T "+THREADS+" -n "+outname
     os.system(raxCmd)
