4f4fb51c2a4c50a2d52a885f1fa7c938edceacd1
Elizabeth McDaniel <emcdaniel@wisc.edu>
2019-10-11 17:34:23 -0500

    fixed stitching together the heatmaps in R, only adds the grid packages, have to check installation, fixes #20

--- bin/make-heatmap.R
+++ bin/make-heatmap.R
@@ -9,6 +9,8 @@
 #####################################################################
 suppressMessages(library(tidyverse))
 suppressMessages(library(reshape2))
+suppressMessages(library(grid))
+suppressMessages(library(gridExtra))
 
 # Arguments passed from summarize-metabolism.py with metabolic stats output, and input from user for metadata and aggregating option to visualize
 
@@ -86,24 +88,43 @@ make.heatmap <- function(stats_table, file.path, heatmap.option, directory){
     # individual melted dfs and plots
     # carbon
     carbon_melted <- melt(carbon, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group))))
-    carbon_plot <- carbon_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black') + scale_fill_gradient(low="gray92", high="brown4") + theme(panel.grid = element_blank(), panel.border = element_blank())
+    carbon_plot <- carbon_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="gray92", high="brown4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
     carbon_plot_formatted <- carbon_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x="Carbon", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    carbon_clean <- carbon_plot_formatted + theme(legend.position="none")
     # nitrogen
     nitrogen_melted <- melt(nitrogen, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group)))) 
-    nitrogen_plot <- nitrogen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black') + scale_fill_gradient(low="gray92", high="dodgerblue4") + theme(panel.grid = element_blank(), panel.border = element_blank())
-    nitrogen_plot_formatted <- nitrogen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x="Nitrogen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    nitrogen_plot <- nitrogen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black', size=0.5,aes(width=1, height=1)) + coord_fixed() + scale_fill_gradient(low="gray92", high="dodgerblue4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+    nitrogen_plot_formatted <- nitrogen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Nitrogen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    nitrogen_clean <- nitrogen_plot_formatted + theme(legend.position="none", axis.title.y=element_blank(), axis.text.y=element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
     # sulfur
     sulfur_melted <- melt(sulfur, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group)))) 
-    sulfur_plot <- sulfur_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black') + scale_fill_gradient(low="gray92", high="darkorchid4") + theme(panel.grid = element_blank(), panel.border = element_blank())
-    sulfur_plot_formatted <- sulfur_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x="Sulfur", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    sulfur_plot <- sulfur_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() + scale_fill_gradient(low="gray92", high="darkorchid4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+    sulfur_plot_formatted <- sulfur_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Sulfur", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    sulfur_clean <- sulfur_plot_formatted + theme(legend.position="none", axis.title.y=element_blank(), axis.text.y=element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
     # oxygen
     oxygen_melted <- melt(oxygen, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group)))) 
-    oxygen_plot <- oxygen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black') + scale_fill_gradient(low="gray92", high="turquoise4") + theme(panel.grid = element_blank(), panel.border = element_blank())
-    oxygen_plot_formatted <- oxygen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x="Oxygen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    oxygen_plot <- oxygen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() + scale_fill_gradient(low="gray92", high="turquoise4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+    oxygen_plot_formatted <- oxygen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Oxygen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    oxygen_clean <- oxygen_plot_formatted + theme(legend.position="none", axis.title.y=element_blank(), axis.text.y=element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
     # hydrogen
     hydrogen_melted <- melt(hydrogen, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group)))) 
-    hydrogen_plot <- hydrogen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black') + scale_fill_gradient(low="gray92", high="darkorange3") + theme(panel.grid = element_blank(), panel.border = element_blank())
-    hydrogen_plot_formatted <- hydrogen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x="Hydrogen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    hydrogen_plot <- hydrogen_melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black', size=0.5,aes(width=1, height=1)) + coord_fixed() + scale_fill_gradient(low="gray92", high="darkorange3") + theme(plot.margin = unit(c(0, 0, 0, 0), "cm"), panel.grid = element_blank(), panel.border = element_blank())
+    hydrogen_plot_formatted <- hydrogen_plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Hydrogen", y=NULL) +  scale_y_discrete(expand=c(0,0))
+    hydrogen_clean <- hydrogen_plot_formatted + theme(legend.position="none", axis.title.y=element_blank(), axis.text.y=element_blank())
+    # stitch together
+    carbon_tmp <- ggplot_build(carbon_clean)
+    nitrogen_tmp <- ggplot_build(nitrogen_clean)
+    sulfur_tmp <- ggplot_build(sulfur_clean)
+    oxygen_tmp <- ggplot_build(oxygen_clean)
+    hydrogen_tmp <- ggplot_build(hydrogen_clean)
+    g1 <- ggplot_gtable(carbon_tmp) ; g2 <- ggplot_gtable(nitrogen_tmp) ; g3 <- ggplot_gtable(sulfur_tmp) ; g4 <- ggplot_gtable(oxygen_tmp) ; g5 <- ggplot_gtable(hydrogen_tmp)
+    n1 <- length(carbon_tmp[["panel"]][["ranges"]][[1]][["x.major_source"]])
+    n2 <- length(nitrogen_tmp[["panel"]][["ranges"]][[1]][["x.major_source"]])
+    n3 <- length(sulfur_tmp[["panel"]][["ranges"]][[1]][["x.major_source"]])
+    n4 <- length(oxygen_tmp[["panel"]][["ranges"]][[1]][["x.major_source"]])
+    n5 <- length(hydrogen_tmp[["panel"]][["ranges"]][[1]][["x.major_source"]])
+    g <- cbind(g1, g2, g3, g4, g5, size="first")
+    ggsave(file=figureOut, plot=g, width=40, height=40, units=c("cm"))
   }
 }
 
